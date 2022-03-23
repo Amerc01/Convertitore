42f35193acaca15895c1aec4afb2d8a9bc240987
Amerc01 <R%69an64Ro>
2022-03-23 22:34:59 +0100

    Prova menu a tendina

--- app/src/main/java/com/example/convertitoreeurodollaro/MainActivity.java
+++ app/src/main/java/com/example/convertitoreeurodollaro/MainActivity.java
@@ -4,14 +4,18 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Array;
 import java.util.Currency;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,24 +25,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-
-/*        final RadioGroup radioGroup = findViewById(R.id.radioGroup);
-        final int b = radioGroup.getCheckedRadioButtonId();
-        final RadioButton r = findViewById(b);
-        final String currency = r.getText().toString();*/
-
-
         Button convertiInEuro = findViewById(R.id.convertiInEuro);
         convertiInEuro.setOnClickListener(view -> {
             onClickIn(view);
         });
 
-        Button convertiDaEuro = findViewById(R.id.convertiDaEuro);
+       /* Button convertiDaEuro = findViewById(R.id.convertiDaEuro);
         convertiDaEuro.setOnClickListener(view -> {
             onClickFrom(view);
-        });
+        });*/
     }
 
     //da valuta a euro
@@ -46,19 +41,32 @@ public class MainActivity extends AppCompatActivity {
         //prendo il valore inserito dall'utente
         EditText input = findViewById(R.id.quantita);
         float quantita = Float.parseFloat(input.getText().toString());
-        String currency = null;
+        final String[] currency = {null};
 
-        RadioButton usdField = findViewById(R.id.USD);
-        RadioButton gbpField = findViewById(R.id.GBP);
-        RadioButton jpyField = findViewById(R.id.JPY);
-        RadioButton chfField = findViewById(R.id.CHF);
 
-        if (usdField.isChecked()) currency = "USD";
-        if (gbpField.isChecked()) currency = "GBP";
-        if (jpyField.isChecked()) currency = "JPY";
-        if (chfField.isChecked()) currency = "CHF";
 
-        String finalCurrency = currency;
+        String [] items = new String[]{"USD", "GBP", "JPY", "CHF"};
+        ArrayAdapter<String> arrayCurrency = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, items);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(arrayCurrency);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                currency[0] = adapterView.getItemAtPosition(i).toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+
+        String finalCurrency = currency[0];
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -69,6 +77,9 @@ public class MainActivity extends AppCompatActivity {
                 //e restituisce il valore del tasso di conversione
 
                 Double rate = request.getRate(finalCurrency);
+
+                TextView result1 = MainActivity.this.findViewById(R.id.test);
+                result1.setText(String.valueOf(finalCurrency));
 
                 Handler handler = new Handler(Looper.getMainLooper());
                 boolean post = handler.post(() -> {
@@ -85,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //da euro a valuta
-    private void onClickFrom(View view) {
+   /* private void onClickFrom(View view) {
         //prendo il valore inserito dall'utente
         EditText input = findViewById(R.id.quantita);
         float quantita = Float.parseFloat(input.getText().toString());
@@ -125,5 +136,5 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }).start();
-    }
+    }*/
 }

f6bc262ac89047aa9c52a24b934c78a2dddfbb52
Matte <maci.matteo2003@gmail.com>
2022-03-24 12:03:10 +0100

    Funzionante con menu a tendina

--- app/src/main/java/com/example/convertitoreeurodollaro/MainActivity.java
+++ app/src/main/java/com/example/convertitoreeurodollaro/MainActivity.java
@@ -8,17 +8,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.lang.reflect.Array;
-import java.util.Currency;
+import java.text.DecimalFormat;
+
 
 public class MainActivity extends AppCompatActivity {
+    final String[] currency = {null};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,22 +29,13 @@ public class MainActivity extends AppCompatActivity {
             onClickIn(view);
         });
 
-       /* Button convertiDaEuro = findViewById(R.id.convertiDaEuro);
+        Button convertiDaEuro = findViewById(R.id.convertiDaEuro);
         convertiDaEuro.setOnClickListener(view -> {
             onClickFrom(view);
-        });*/
-    }
-
-    //da valuta a euro
-    private void onClickIn(View view) {
-        //prendo il valore inserito dall'utente
-        EditText input = findViewById(R.id.quantita);
-        float quantita = Float.parseFloat(input.getText().toString());
-        final String[] currency = {null};
+        });
 
 
-
-        String [] items = new String[]{"USD", "GBP", "JPY", "CHF"};
+        String[] items = new String[]{"USD", "GBP", "JPY", "CHF"};
         ArrayAdapter<String> arrayCurrency = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, items);
         Spinner spinner = findViewById(R.id.spinner);
         spinner.setAdapter(arrayCurrency);
@@ -59,12 +49,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+                currency[0] = "USD";
 
             }
         });
+    }
 
-
-
+    //da valuta a euro
+    private void onClickIn(View view) {
+        //prendo il valore inserito dall'utente
+        EditText input = findViewById(R.id.quantita);
+        float quantita = Float.parseFloat(input.getText().toString());
 
         String finalCurrency = currency[0];
         new Thread(new Runnable() {
@@ -78,14 +73,12 @@ public class MainActivity extends AppCompatActivity {
 
                 Double rate = request.getRate(finalCurrency);
 
-                TextView result1 = MainActivity.this.findViewById(R.id.test);
-                result1.setText(String.valueOf(finalCurrency));
-
                 Handler handler = new Handler(Looper.getMainLooper());
                 boolean post = handler.post(() -> {
                     //modifica all'interfaccia
                     //calcolo il valore in euro
-                    Double euro =  quantita / rate;
+                    DecimalFormat format = new DecimalFormat("####.000");
+                    Double euro = Double.parseDouble(format.format(quantita / rate));
 
                     TextView result = MainActivity.this.findViewById(R.id.resultField);
                     result.setText(String.valueOf(euro));
@@ -95,24 +88,14 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    //da euro a valuta
-   /* private void onClickFrom(View view) {
+
+    //da valuta a euro
+    private void onClickFrom(View view) {
         //prendo il valore inserito dall'utente
         EditText input = findViewById(R.id.quantita);
         float quantita = Float.parseFloat(input.getText().toString());
-        String currency = null;
 
-        RadioButton usdField = findViewById(R.id.USD);
-        RadioButton gbpField = findViewById(R.id.GBP);
-        RadioButton jpyField = findViewById(R.id.JPY);
-        RadioButton chfField = findViewById(R.id.CHF);
-
-        if (usdField.isChecked()) currency = "USD";
-        if (gbpField.isChecked()) currency = "GBP";
-        if (jpyField.isChecked()) currency = "JPY";
-        if (chfField.isChecked()) currency = "CHF";
-
-        String finalCurrency = currency;
+        String finalCurrency = currency[0];
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -124,17 +107,21 @@ public class MainActivity extends AppCompatActivity {
 
                 Double rate = request.getRate(finalCurrency);
 
+                /*TextView result1 = MainActivity.this.findViewById(R.id.test);
+                result1.setText(String.valueOf(finalCurrency));*/
+
                 Handler handler = new Handler(Looper.getMainLooper());
                 boolean post = handler.post(() -> {
                     //modifica all'interfaccia
                     //calcolo il valore in euro
-                    Double valuta =  quantita * rate;
+                    DecimalFormat format = new DecimalFormat("####.000");
+                    Double euro = Double.parseDouble(format.format(quantita * rate));
 
                     TextView result = MainActivity.this.findViewById(R.id.resultField);
-                    result.setText(String.valueOf(valuta));
+                    result.setText(String.valueOf(euro));
 
                 });
             }
         }).start();
-    }*/
+    }
 }
